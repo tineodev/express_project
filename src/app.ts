d2a6a3c01f2cf34b6c7fbacfdeb3be0b9e455195
Alvaro <ixml3d@gmail.com>
2023-01-19 03:17:12 -0500

    feature: add component user

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 import express, { type Application } from "express";
+import { userRouter } from "./components";
+
+
 
 const app: Application = express();
+
+app.use(express.json());
+
+app.use("/api/v1/users", userRouter);
 
 export default app;
