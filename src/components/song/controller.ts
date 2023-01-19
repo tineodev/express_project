727a9f24c66e442551e947d7af8976d93672d5f6
tineodev <tineo.dev@gmail.com>
2023-01-19 10:55:28 -0500

    Add method post

--- src/components/song/controller.ts
+++ src/components/song/controller.ts
@@ -3,4 +3,18 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+export const listSong = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const data = req.body;
 
+    const element = await prisma.user.create({ data });
+
+    res.status(201).json({
+      ok: true,
+      message: "Usuario creado correctamente",
+      element: element,
+    });
+  } catch (error) {
+    res.status(500).json({ ok: false, message: error });
+  }
+};
