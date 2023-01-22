f930f9e24152c2a2adfa4904e38cb7b6b17386fc
tineodev <tineo.dev@gmail.com>
2023-01-22 09:09:03 -0500

    Conditional access

--- src/components/song/controller.ts
+++ src/components/song/controller.ts
@@ -1,9 +1,7 @@
 import type { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
-/*
-var userC = require('../user/controller')
-console.log(userC.accessUser); //* true | false
-*/
+
+var userLogin = require("../user/controller");
 
 const prisma = new PrismaClient();
 
@@ -26,7 +24,7 @@ export const postSong = async (req: Request, res: Response): Promise<void> => {
     res.status(201).json({
       ok: true,
       message: "Song successfully created",
-      element: element,
+      results: element,
     });
   } catch (error) {
     res.status(500).json({ ok: false, message: error });
@@ -35,12 +33,27 @@ export const postSong = async (req: Request, res: Response): Promise<void> => {
 
 export const getSong = async (req: Request, res: Response): Promise<void> => {
   try {
-    const element = await prisma.song.findMany({
-      where: {
-        public: true,
-      },
-    });
-    res.status(200).json(element);
+    if (userLogin.accessUser) {
+      const element = await prisma.song.findMany({
+        where: {},
+      });
+      res.status(200).json({
+        ok: true,
+        message: "Private and public songs",
+        results: element,
+      });
+    } else {
+      const element = await prisma.song.findMany({
+        where: {
+          public: true,
+        },
+      });
+      res.status(200).json({
+        ok: true,
+        message: "Only public songs (private songs require login)",
+        results: element,
+      });
+    }
   } catch (error) {
     res.status(500).json({
       ok: false,
@@ -59,7 +72,10 @@ export const getSongID = async (req: Request, res: Response) => {
       },
     });
     if (element) {
-      res.status(200).json(element);
+      res.status(200).json({
+        ok: true,
+        results: element,
+      });
     }
     res.status(404).json({
       ok: false,
